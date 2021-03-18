1a1f7d43e7a07c65e50323d38fee1ea5b80331fc
Sergio Diosdado <sgdc_98@hotmail.com>
2021-03-18 11:00:40 -0500

    Add ChakraUI framework

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "@chakra-ui/react"
 
 function App() {
   return (
@@ -9,14 +10,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button colorScheme="blue">
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+        </Button>
       </header>
     </div>
   );
