bcd056f49bb81b01f3eb978da57971cc72a087d0
Sergio Diosdado <sgdc_98@hotmail.com>
2021-03-24 15:22:08 -0500

    Add more models and new fixtures. Add custom AutoDateTimeField

--- doctory/core/models.py
+++ doctory/core/models.py
@@ -3,8 +3,12 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models.fields.related import OneToOneField
 
-from .constants import UserTypes, BackgroundTypes, BackgroundSubtypes
+from .utils import AutoDateTimeField
+from django.utils import timezone
 
+class UserTypes(models.TextChoices):
+    PACIENT = 'PAC', 'Pacient'
+    MEDIC = 'MED', 'Medic'
 
 def set_default_user_type():
     return [UserTypes.PACIENT]
@@ -13,6 +17,8 @@ def set_default_user_type():
 class User(AbstractUser):
 
     type = ArrayField(models.CharField(max_length=50, choices=UserTypes.choices), default=set_default_user_type)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = AutoDateTimeField(default=timezone.now, editable=False)
 
     def __str__(self):
         return self.username
@@ -62,15 +68,66 @@ class Medic(User):
 
 class PacientMore(models.Model):
     user = OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = AutoDateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = AutoDateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.name
 
 
 class MedicMore(models.Model):
     user = OneToOneField(User, on_delete=models.CASCADE)
     license = models.CharField(max_length=20, blank=True, null=True)
+    specialties = models.ManyToManyField(Specialty, through='MedicSpecialty', related_name='medics')
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = AutoDateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class MedicSpecialty(models.Model):
+    medic = models.ForeignKey(MedicMore, on_delete=models.CASCADE)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+
+
+class BackgroundType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = AutoDateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class BackgroundSubtype(models.Model):
+    background_type = models.ForeignKey(BackgroundType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True) 
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = AutoDateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Condition(models.Model):
+    background_subtype = models.ForeignKey(BackgroundSubtype, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    background_type = models.CharField(max_length=50, choices=BackgroundTypes.choices)
-    background_subtype = models.CharField(max_length=50, choices=BackgroundSubtypes.choices)
+    date_of_diagnosis = models.DateTimeField()
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = AutoDateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.name
