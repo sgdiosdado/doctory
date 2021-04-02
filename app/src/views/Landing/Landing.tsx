1fdb46bf413e399312df5f768519fd6865496bc7
Raul <raulcastellanosh@hotmail.com>
2021-04-02 15:29:41 -0600

    Added assets

--- app/src/views/Landing/Landing.tsx
+++ app/src/views/Landing/Landing.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@chakra-ui/button'
+import { useColorModeValue } from '@chakra-ui/color-mode'
+import { Container } from '@chakra-ui/layout'
 import React from 'react'
 
 export const Landing = () => {
     return (
-        <div>
+        <Container
+            
+            m={0}
+        >
             <h1>Landing page!!!!</h1>
-        </div>
+            <Button >
+                Holaa
+            </Button>
+        </Container>
     )
 }
