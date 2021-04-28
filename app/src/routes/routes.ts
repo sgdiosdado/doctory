fdf5f408f8a1bb4d87b3abef6487dbec3c6206e6
Sergio Diosdado <sgdc_98@hotmail.com>
2021-04-28 18:36:53 -0500

    Change routes.tsx extension

--- app/src/routes/routes.ts
+++ app/src/routes/routes.ts
@@ -10,25 +10,25 @@ type route = {
   component: React.FC,
 }
 
-const patientPaths: {[name:string]: route} = {
-  patientHome: {path: '/pat/home', component: PatientHomeView},
-  patientProfile: {path: '/pat/profile', component: NotfoundPage},
+const patientPaths: { [name: string]: route } = {
+  patientHome: { path: '/pat/home', component: PatientHomeView },
+  patientProfile: { path: '/pat/profile', component: NotfoundPage },
 }
 
-const doctorPaths: {[name:string]: route} = {
-  doctorLanding: {path: '/med/landing', component: DoctorLandingView},
-  doctorpatients: {path: '/med/patients', component: NotfoundPage },
+const doctorPaths: { [name: string]: route } = {
+  doctorLanding: { path: '/med/landing', component: DoctorLandingView },
+  doctorpatients: { path: '/med/patients', component: NotfoundPage },
 }
 
-const generalPaths: {[name:string]: route} = {
+const generalPaths: { [name: string]: route } = {
   default: { path: '/', component: Landing },
   login: { path: '/login', component: LoginView },
   signin: { path: '/signup', component: SignUpView },
   contact: { path: '/contact', component: NotfoundPage },
-  notFound: {path: '/404', component: NotfoundPage },
+  notFound: { path: '/404', component: NotfoundPage },
 }
 
-export const routes:{[name:string]: route} = {
+export const routes: { [name: string]: route } = {
   ...generalPaths,
   ...patientPaths,
   ...doctorPaths,
