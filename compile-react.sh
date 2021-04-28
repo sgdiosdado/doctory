0bb55a397e89ed772da5128b377b6b1a3f678ec9
Sergio Diosdado <sgdc_98@hotmail.com>
2021-04-28 18:06:52 -0500

    Add SECRET_KEY to env file. Modified heroku.yml and create compile-react script

---
+++ compile-react.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ./app && yarn relocate && cd ..
+
--- doctory/doctory/settings.py
+++ doctory/doctory/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y1ar^6-_o+3@r=bajv#x=(+9r(0f1@ewd8qkayq83+7w+_mu$a'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ['DEBUG']
