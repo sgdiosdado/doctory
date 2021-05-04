4bbd08d30e6d303bd1f083ebce52bb8ffe18eba0
Raul <raulcastellanosh@hotmail.com>
2021-05-04 00:28:18 -0500

    Add front routes protection

--- app/src/provider/AuthProvider.tsx
+++ app/src/provider/AuthProvider.tsx
@@ -1,60 +1,68 @@
 import React, { useState, FC, useEffect } from 'react';
 import { createContext } from "react";
+import { http } from '../http/client';
 import { getToken } from '../utils/token';
 
 
 type authContextStateType = {
-  type: string[],
+  type?: string[],
   isLoggedIn: boolean,
+  isLoading: boolean,
 }
 
 type authContextType = {
   authContext: authContextStateType,
   login: (type: string[]) => void,
-  setType: (type: string[]) => void,
   logout: () => void,
 }
 
 const defaultContextValues: authContextType = {
-  authContext: { type: [''], isLoggedIn: false },
+  authContext: { type: [''], isLoggedIn: false, isLoading: true },
   login: () => {},
   logout: () => {},
-  setType: () => {},
 }
 // Context
 export const UserContext = createContext<authContextType>(defaultContextValues);
 
 // Provider
 export const AuthProvider : FC = ({ children }) => {
-  const [authContext, setAuthContext] = useState<authContextStateType>({ type: [''], isLoggedIn: true });
+  
+  const [authContext, setAuthContext] = useState<authContextStateType>(defaultContextValues.authContext);
 
   const login = (type : string[]) => {
     setAuthContext(_ => ({
-      type: type,
+      type,
       isLoggedIn: true,
+      isLoading: false,
     }));
   };
-
-  const setType = (type: string[]) => {
-    setAuthContext(x =>({...x, type:type}))
-  }
 
   const logout = () => {
     setAuthContext(_ => ({
       type: [''],
       isLoggedIn: false,
+      isLoading: false,
     }));
   };
 
   useEffect(() => {
-    const storedData = getToken();
-    if (storedData) {
-       login(['PAT']); //TODO
+    if(getToken()) { // If there's already a Token in storage we validate it
+      http.getUserType().then(type => {
+        if(!type) {
+          localStorage.clear();
+          setAuthContext({type:undefined, isLoggedIn: false, isLoading: false})
+        }else{
+          setAuthContext({type:type, isLoggedIn: true, isLoading: false})
+        }
+      })
+    }else{
+      setAuthContext({type:undefined, isLoggedIn: false,  isLoading: false})
     }
+
   }, []);
 
   return (
-    <UserContext.Provider value={{ authContext, login, logout, setType }}>
+    <UserContext.Provider value={{ authContext, login, logout }}>
       {children}
     </UserContext.Provider>
   );
--- app/src/routes/AppRouter.tsx
+++ app/src/routes/AppRouter.tsx
@@ -1,15 +1,15 @@
 import { Box } from '@chakra-ui/layout';
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import { Navbar } from '../views/Navbar/Navbar';
-import { routes } from './routes';
+import { protectedRoutes, publicRoutes } from './routes';
 import { useColorModeValue } from '@chakra-ui/color-mode';
 import { Footer } from '../views/Footer/Footer';
-
+import { UserContext } from '../provider/AuthProvider';
+import { LoadingView } from '../views/AuthViews/LoadingView';
 
 export const AppRouter = () => {
-
-
+  const { authContext } = useContext(UserContext)
 
   return (
       <Box d={'flex'} alignItems={'center'} flexDirection={'column'} minH={'100vh'}>
@@ -18,14 +18,24 @@ export const AppRouter = () => {
             <Navbar />
           </Box>
           <Box h='100%' width='100%' d='flex' flexGrow={1} flexDirection={'column'} bg={useColorModeValue('gray.100', 'gray.800')}>
-            <Switch>
-              {
-                Object.keys(routes).map(k => (
-                  <Route key={k} exact={k !== '404'} path={routes[k].path} component={routes[k].component}/>
-                ))
+              { authContext.isLoading
+                ? <LoadingView />
+                : (
+                  <Switch>
+                  {authContext.isLoggedIn &&
+                  Object.keys(protectedRoutes).map(k => (
+                    authContext.isLoggedIn
+                    ? <Route key={k} exact path={protectedRoutes[k].path} component={protectedRoutes[k].component} />
+                    : <Redirect key={k} to={publicRoutes.login.path} />
+                  ))}
+
+                  {Object.keys(publicRoutes).map(k => (
+                    <Route key={k} exact={k !== '404'} path={publicRoutes[k].path} component={publicRoutes[k].component}/>
+                  ))}
+                  <Redirect to={publicRoutes.notFound.path} />
+                  </Switch>
+                )
               }
-              <Redirect to={routes.notFound.path} />
-            </Switch>
             <Footer />
           </Box>
         </Router>
